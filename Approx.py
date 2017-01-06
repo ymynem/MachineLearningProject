2bfc0b804ac80288bf99f6ef6f990be2f0f0a13c
Fhuss <fabian.huss@gmail.com>
2017-01-06 10:55:30 +0100

    alignment from definition 3 done, updated approx

--- Approx.py
+++ Approx.py
@@ -1,11 +1,22 @@
 from collections import Counter
 
 def Sorting(n,x,inputfile):
+    """
+    n = length of string/word needed for subset
+    x = how many most frequently occuring n length words
+    inputfile = dataset used for algorithm
+    
+    Returns a list of x most frequently occuring n length caracters in a file
+    """
     f=open(inputfile,'r')
-    S=f.read()
-    mapping = [ ('.', ''), (',', ''), ('?', ''), ('!', ''), ('%', ''),('\n','') ]
+    S=f.read().lower()
+    mapping = [ ('.', ''), (',', ''), ('?', ''), ('!', ''), ('%', ''),('\n',''),('\t','')]
     for i,j in mapping:
         S=S.replace(i,j)
     S=S.split(' ')
     subset = [s for s in S if len(s) == n]
-    return Counter(subset).most_common(x)
+    mst_cmn=Counter(subset).most_common(x) 
+    mst_cmn=[x[0] for x in mst_cmn]
+    
+    return mst_cmn
+    
