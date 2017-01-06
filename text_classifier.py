6f6796d9d282d24be4adc1592bc288c906fbee72
Janet Jie Deng <janetjied@gmail.com>
2017-01-06 22:46:29 +0100

    Improve buildGramMat(). & Using reuters data.

--- text_classifier.py
+++ text_classifier.py
@@ -4,6 +4,7 @@ from sklearn.svm import libsvm
 from sklearn.datasets import fetch_20newsgroups
 
 from ssk_by_Mona import normalize
+from reuters import *
 
 
 # build kernel matrix of string list s and string list t
@@ -15,8 +16,16 @@ def buildGramMat(sList, tList, l, n):
 	print("Hi")
 	for i in range(lenS):
 		for j in range(lenT):
-			gramMat[i, j] = normalize(sList[i], tList[j], l, n)  # here to calculate the ssk value
-			print ('normalize done!')
+			if i == j:
+				gramMat[i][j] = 1
+				print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
+			elif gramMat[j][i] != 0:
+				gramMat[i][j] = gramMat[j][i]
+				print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
+			else:
+				gramMat[i, j] = normalize(sList[i], tList[j], l, n)  # here to calculate the ssk value
+				print ('normalize done!')
+				print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
 
 	return gramMat
 
@@ -38,18 +47,36 @@ def predict(svc, dataTest, dataTrain, l, n):
 	return svc.predict(gramMat)
 
 
-def textClassify():
-	trainSize = 2
-	testSicze = 2
+def textClassify(cat1, cat2):
+	#trainSize = 2
+	#testSicze = 2
 
 	n = 2
 	l = 0.5
 
+	'''
 	news = fetch_20newsgroups(subset='train')
 	trainX = news.data[:trainSize]
 	trainY = news.target[:trainSize]
+	'''
+
+	# train the SVC
+	a_train, a_test = get_documents(cat1)
+	b_train, b_test = get_documents(cat2)
+
+	print("Number of documents:", len(a_train), len(b_train))
+
+	trainX = create_corpus(a_train + b_train)
+	trainY = [cat1]*len(a_train) + [cat2]*len(b_train)
+
+	print trainX[0]
 
 	svc = train(trainX, trainY, l, n)
-	return predict(svc, news.data[trainSize:trainSize+testSize], trainX, l, n)     
 
-textClassify()
+	# classify
+	testX = create_corpus(a_test + b_test)
+	testY = [cat1]*len(a_test) + [cat2]*len(b_train)
+
+	return predict(svc, testX, trainX, l, n)     
+
+# textClassify()
