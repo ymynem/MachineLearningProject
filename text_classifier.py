97553d99d7b26c65822eb6cc15e518efba3a84a9
Janet Jie Deng <janetjied@gmail.com>
2017-01-07 15:46:34 +0100

    Commits added to easier understand of text_classifier.

--- text_classifier.py
+++ text_classifier.py
@@ -5,7 +5,7 @@ from ssk_by_Mona import normalize
 from reuters import *
 
 
-# build kernel matrix of string list s and string list t
+# build kernel matrix of string list s and string list t with ssk
 def buildGramMat(sList, tList, l, n):
 	lenS = len(sList)
 	lenT = len(tList)
@@ -14,6 +14,7 @@ def buildGramMat(sList, tList, l, n):
 	gramMat[:] = -1
 
 	# optimize calculation of kernel gram matrix when sList equals to tList
+	# in our case, this is to save calculation for kernel gram matrix of training data
 	if sList is tList:
 		print("Hi")
 		for i in range(lenS):
@@ -24,6 +25,8 @@ def buildGramMat(sList, tList, l, n):
 				elif gramMat[i][j] == -1:
 					gramMat[i][j] = gramMat[j][i] = normalize(sList[i], tList[j], l, n)  # here to calculate the ssk value
 					print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
+	# without the two list equal to one another, we have to calculate every element for gram matrix
+	# in our case, this is for kernel gram matrix of training data and test data
 	else:
 		for i in range(lenS):
 			for j in range(lenT):
@@ -33,11 +36,12 @@ def buildGramMat(sList, tList, l, n):
 	return gramMat
 
 
+# train svc with kernel gram matrix of training data
 def train(data, label, l, n):
-	# build kernel matrix of training data
+	# build kernel gram matrix of training data
 	gramMat = buildGramMat(data, data, l, n)
 
-	# train support vector classification
+	# train support vector classification (svc) with above gram matrix built
 	svc = svm.SVC(kernel='precomputed')
 	svc.fit(gramMat, label)
 	
@@ -46,24 +50,28 @@ def train(data, label, l, n):
 
 # predict new dataset
 def predict(svc, dataTest, dataTrain, l, n):
+	# build kernel gram matrix of training data and test data
 	gramMat = buildGramMat(dataTest, dataTrain, l, n)
 	return svc.predict(gramMat)
 
 
+# retrieve and clean reuters data, wrap up method of train and predict
 def textClassify(cat1, cat2):
 	n = 2
 	l = 0.5
 
 	# train the SVC using reuters datasets
+	# get raw doc
 	a_train, a_test = get_documents(cat1)
 	b_train, b_test = get_documents(cat2)
 
 	print("Number of documents:", len(a_train), len(b_train))
 
+	# clean the doc
 	trainX = create_corpus(a_train + b_train)
 	trainY = [cat1]*len(a_train) + [cat2]*len(b_train)
 
-	#print trainX[0]   # print the fisrt training text
+	#print trainX[0]   # print the fisrt training text for test
 
 	svc = train(trainX, trainY, l, n)
 
@@ -73,4 +81,4 @@ def textClassify(cat1, cat2):
 
 	return predict(svc, testX, trainX, l, n)     
 
-# textClassify()
+# textClassify('acq', 'corn')
