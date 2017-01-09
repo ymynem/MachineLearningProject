57891e3b322c73e3f96b4b596a63ce3f9c9cd94d
monadadoun <dadoun@kth.se>
2017-01-09 18:14:05 +0100

    now we are partitioning data and predicting

--- subset_creator.py
+++ subset_creator.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from reuters import *
 import re
+
 """
 Approximating string kernel using the most common occurring
 strings in documents. This function creates a subset of the full
@@ -13,54 +14,56 @@ Example:
 Author: Fabian Huss
 """
 
-def sorting(n,training, testing):
+
+def sorting(n, training, testing):
     """
     n: length of string/word for subset
     category: which category in reuters to be used
     
     Returns a list of x most frequently occuring n length characters in a file
     """
-    
-    ltrain=len(training)
-    ltest=len(testing)
-    train_sub=[]
-    test_sub=[]
-    test=[]
-    train=[]
+
+    ltrain = len(training)
+    ltest = len(testing)
+    train_sub = []
+    test_sub = []
+    test = []
+    train = []
     for i in range(ltrain):
         train.append(training[i].split())
     for j in range(ltest):
         test.append(testing[j].split())
-    
+
     for i in range(len(train)):
-        train_sub.append([s for s in train[i] if len(s)==n])
+        train_sub.append([s for s in train[i] if len(s) == n])
     for j in range(len(test)):
-        test_sub.append([s for s in test[j] if len(s)==n])
-    
+        test_sub.append([s for s in test[j] if len(s) == n])
+
     return train_sub, test_sub
 
-def list_to_string(train,test):
-    
-    training=[]
-    testing=[]
+
+def list_to_string(train, test):
+    training = []
+    testing = []
     for i in range(len(train)):
         training.append(' '.join(train[i]))
     for j in range(len(test)):
         testing.append(' '.join(test[j]))
     return training, testing
 
-def most_common(n,x,train,test):
+
+def most_common(n, x, train, test):
     """
     x: how many most frequently occurring n length words
     
     """
-    train, test = sorting(n,train, test)
-    
-    train_cmn=[]
-    test_cmn=[]
-    ltrain=len(train)
-    ltest=len(test)
-    
+    train, test = sorting(n, train, test)
+
+    train_cmn = []
+    test_cmn = []
+    ltrain = len(train)
+    ltest = len(test)
+
     """
     appends the x most frequently used n length words to train_cmn/test_cmn
     for each document
@@ -70,15 +73,10 @@ def most_common(n,x,train,test):
     for j in range(ltest):
         test_cmn.append(Counter(test[j]).most_common(x))
     for i in range(ltrain):
-        train_cmn[i]=[x[0] for x in train_cmn[i]]
+        train_cmn[i] = [x[0] for x in train_cmn[i]]
     for j in range(ltest):
-        test_cmn[j]=[x[0] for x in test_cmn[j]]
-    
-    train_cmn,test_cmn=list_to_string(train_cmn,test_cmn)
-    
-        
-    return train_cmn,test_cmn
-    
+        test_cmn[j] = [x[0] for x in test_cmn[j]]
 
-    
-    
+    train_cmn, test_cmn = list_to_string(train_cmn, test_cmn)
+
+    return train_cmn, test_cmn
--- test.py
+++ test.py
@@ -4,34 +4,37 @@ from sklearn import svm
 from reuters import *
 from subset_creator import *
 import cProfile
-import time
 import random
 
 
 # build kernel matrix of string list s and string list t with ssk
 def buildGramMat(sList, tList, l, n):
+    print("buildGramMat()")
     # slist = train
     # tlist = test
     lenS = len(sList)
     lenT = len(tList)
+    gramMat = np.zeros((lenS, lenT), dtype=np.float64)
+    gramMat[:] = -1  # fyller med -1or
+
     # optimize calculation of kernel gram matrix when sList equals to tList
     # in our case, this is to save calculation for kernel gram matrix of training data
     if sList is tList:  # sList is the same object as tList
-        k = 1
-        gramMat = np.eye(lenS, lenT, dtype=np.float64)
         for i in range(lenS):
-            for j in range(k, lenT):
-                gramMat[i][j] = gramMat[j][i] = normalize(sList[i], tList[j], l, n)
-            k += 1  # here to calculate the ssk value
-            # print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
-            # without the two list equal to one another, we have to calculate every element for gram matrix
-            # in our case, this is for kernel gram matrix of training data and test data
+            for j in range(lenT):
+                if i == j:
+                    gramMat[i][j] = 1
+                    # print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
+                elif gramMat[i][j] == -1:
+                    gramMat[i][j] = gramMat[j][i] = normalize(sList[i], tList[j], l,
+                                                              n)  # here to calculate the ssk value
+                    # print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
+                    # without the two list equal to one another, we have to calculate every element for gram matrix
+                    # in our case, this is for kernel gram matrix of training data and test data
     else:
-        gramMat = np.zeros((lenS, lenT), dtype=np.float64)
         for i in range(lenS):
             for j in range(lenT):
                 gramMat[i][j] = normalize(sList[i], tList[j], l, n)  # here to calculate the ssk value
-
                 # print("gramMat[{}][{}] = {}".format(i, j, gramMat[i][j]))
 
     return gramMat
@@ -39,17 +42,22 @@ def buildGramMat(sList, tList, l, n):
 
 # train svc with kernel gram matrix of training data
 def train(data, label, l, n):
+    print("train()")
     # build kernel matrix of training data
     gramMat = buildGramMat(data, data, l, n)
 
     # train support vector classification (svc) with above gram matrix built
     svc = svm.SVC(kernel='precomputed')
+    """"A column-vector y was passed when a 1d array was"
+                          " expected. Please change the shape of y to "
+                          "(n_samples, ), for example using ravel().","""
     svc.fit(gramMat, label)  # np.ravel(label)
     return svc
 
 
 # predict new dataset
 def predict(svc, dataTest, dataTrain, l, n):
+    print("predict()")
     # build kernel gram matrix of training data and test data
     gramMat = buildGramMat(dataTest, dataTrain, l,
                            n)  # G[i][j] = K(a[i],b[j]) a,b är list of documents, our inputs to the ssk kernel
@@ -68,42 +76,37 @@ def textClassify(cat1, cat2):
     l = 0.5
     word_frqu = 3
     len_of_word = 6
-    fraction = 1 / 30
+    fraction = 1/30
 
     # train the SVC using reuters datasets
     a_train, a_test = get_documents(cat1)
     b_train, b_test = get_documents(cat2)
-    a_train_first_part, second = partition(a_train, 1 / 40)
-    # print("a_train", len(a_train_first_part))
-    a_test_first_part, sec_p = partition(a_test, 1 / 30)
-    # print("a_test", len(a_test_first_part))
-    b_train_first_part, se_p = partition(b_train, 1 / 6)
-    # print("b_train", len(b_train_first_part))
-    b_test_first_part, s_p = partition(b_test, 1 / 3)
-    # print("b_test", len(b_test_first_part))
+    a_train_first_part, second = partition(a_train, 1/40)
+    #print("a_train", len(a_train_first_part))
+    a_test_first_part, sec_p = partition(a_test, 1/30)
+    #print("a_test", len(a_test_first_part))
+    b_train_first_part, se_p = partition(b_train, 1/6)
+    #print("b_train", len(b_train_first_part))
+    b_test_first_part, s_p = partition(b_test, 1/3)
+    #print("b_test", len(b_test_first_part))
 
     testX = create_corpus(a_test_first_part + b_test_first_part)
     trainX = create_corpus(a_train_first_part + b_train_first_part)
 
     trainX_most_cm, testX_most_cm = most_common(len_of_word, word_frqu, trainX, testX)
 
-    trainY = [cat1] * len(a_train_first_part) + [cat2] * len(
-        b_train_first_part)  # lista av orden acq och en lista av "corn"
+    trainY = [cat1] * len(a_train_first_part) + [cat2] * len(b_train_first_part)  # lista av orden acq och en lista av "corn"
     testY = [cat1] * len(a_test_first_part) + [cat2] * len(b_test_first_part)  # skapar testdata
     svc = train(trainX_most_cm, trainY, l, n)
-    # print("testY ", testY)
-    pr = predict(svc, testX_most_cm, trainX_most_cm, l, n)
-    total_correct = len([1 for p in zip(pr, testY) if p[0] == p[1]])
-    print("Results: {}/{} - {:.2f}%".format(total_correct, len(pr), 100*total_correct/len(pr)))
+    #print("testY ", testY)
+    return predict(svc, testX_most_cm, trainX_most_cm, l, n)
 
-
-    return pr
 
 
 if __name__ == "__main__":
     # reuters.download()
 
-    # textClassify()
+    #textClassify('acq', 'corn')
     print(textClassify('acq', 'corn'))
-    #print("DONE")
-    #cProfile.run('textClassify("acq", "corn")')
+    print("DONE")
+    #cProfile.run('textClassify()')
