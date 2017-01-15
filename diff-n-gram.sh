1cc9de93d6416eecf4d055d76db7475e882820d9
Þorsteinn Daði Gunnarsson <tdgu@kth.se>
2017-01-15 19:36:06 +0100

    output files to folders

--- diff-n-gram.sh
+++ diff-n-gram.sh
@@ -1,6 +1,8 @@
+mkdir -p diffngram
+
 for n in 3 4 5 6 7 8 10 12 14
 do
     echo $n
-    time python3 classify.py ngram -n $n -i 2  > $ngrami2.txt
+    time python3 classify.py ngram -n $n -i 2  > diffngram/$ngrami2.txt
 done
 
--- diff-n.sh
+++ diff-n.sh
@@ -1,6 +1,8 @@
+mkdir -p diffn
+
 for n in 3 4 5 6 7 8 10 12 14
 do
     echo $n
-    time python3 classify.py ssk -n $n -i 2 -l 0.5 > n$ni2l0.5.txt
+    time python3 classify.py ssk -n $n -i 2 -l 0.5 > diffn/n$ni2l0.5.txt
 done
 
