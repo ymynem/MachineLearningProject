41eff5ad1e6c05d2e987e469c99f64a911d2ae05
monadadoun <dadoun@kth.se>
2017-01-15 15:13:37 +0100

    the data

--- Approx.py
+++ Approx.py
@@ -1,22 +1,79 @@
-from collections import Counter
+import numpy as np
+from ssk import ssk
+from itertools import product
+from collections import defaultdict
+from random import shuffle
 
-def Sorting(n,x,inputfile):
+
+def enumerate_substring(s, alphabet):
     """
-    n = length of string/word needed for subset
-    x = how many most frequently occuring n length words
-    inputfile = dataset used for algorithm
-    
-    Returns a list of x most frequently occuring n length caracters in a file
+    aaa
+    aab
+    aac
+    ..
+    aba
+    abb
+    ..
+    baa
     """
-    f=open(inputfile,'r')
-    S=f.read().lower()
-    mapping = [ ('.', ''), (',', ''), ('?', ''), ('!', ''), ('%', ''),('\n',''),('\t','')]
-    for i,j in mapping:
-        S=S.replace(i,j)
-    S=S.split(' ')
-    subset = [s for s in S if len(s) == n]
-    mst_cmn=Counter(subset).most_common(x) 
-    mst_cmn=[x[0] for x in mst_cmn]
+    i = 0
+    for j in range(len(s)):
+       i += (len(alphabet)**(len(s)-j-1))*alphabet.index(s[j])
+    return i
+
+
+def get_S(n):
+    return list("".join(c) for c in product(*(["abcdefghijklmnopqrstuvwxyz "]*n)))
+
+
+def get_K(S):
+    return lambda s, t, n, l: approx_ssk(s, t, n, l, S)
+
+
+def approx_ssk(s, t, n, l, S):
+    #print("!!")
+    return sum(ssk(s, si, n, l)*ssk(t, si, n, l) for si in S)
+
+
+def get_S_from_corpus(corpus, n, alphabet="abcdefghijklmnopqrstuvwxyz "):
+    fs = defaultdict(lambda: 0)
+    for d in corpus:
+        for i in range(len(d)-n):
+            #index = enumerate_substring(d[i:i+n], alphabet)
+            index = d[i:i+n]
+            fs[index] += 1
     
-    return mst_cmn
-    
+    return [(k, v) for k, v in fs.items()]
+
+
+def get_top_S(corpus, n, count=200, alphabet="abcdefghijklmnopqrstuvwxyz "):
+    fs_list = get_S_from_corpus(corpus, n)
+    fs_list.sort(key=lambda p: p[1], reverse=True)  # Sort after count
+    subs = [p[0] for p in fs_list[:count]]
+    return subs
+
+
+def get_worst_S(corpus, n, count=200, alphabet="abcdefghijklmnopqrstuvwxyz "):
+    fs_list = get_S_from_corpus(corpus, n)
+    fs_list.sort(key=lambda p: p[1], reverse=False)  # Sort after count
+    subs = [p[0] for p in fs_list[:count]]
+    return subs
+
+
+def get_random_S(corpus, n, count=200, alphabet="abcdefghijklmnopqrstuvwxyz "):
+    fs_list = get_S_from_corpus(corpus, n)
+    shuffle(fs_list)
+    subs = [p[0] for p in fs_list[:count]]
+    return subs
+
+
+if __name__ == "__main__":
+    assert enumerate_substring("aaa", "abcdef") == 0
+    assert enumerate_substring("aab", "abcdef") == 1
+    assert enumerate_substring("aac", "abcdef") == 2
+    assert enumerate_substring("aba", "abcdef") == 6
+    assert enumerate_substring("abb", "abcdef") == 7
+    assert enumerate_substring("aff", "abcdef") == 6**2-1
+    assert enumerate_substring("baa", "abcdef") == 6**2
+    assert enumerate_substring("fff", "abcdef") == 6**3-1
+
